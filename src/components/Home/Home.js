53bed6e18c0ae79cdf76f6c7ab7c136e6ccc025f
ARFAT-HASAAN <arfuhtv757@gmail.com>
2022-10-17 16:15:19 +0600

    navbar change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,17 +7,70 @@ import Footer from '../Footer'
 import Navbar from '../Navbar'
 import Skills from '../Skills'
 import Work from '../Work'
-
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedinIn,
+  FaEnvelopeOpen,
+  FaFile,
+} from 'react-icons/fa'
 const Home = () => {
   return (
     <>
+      <div className=" hidden  md:flex fixed flex-col left-0 top-80">
+        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300 py-2 bg-blue-500 ">
+          <a
+            href="https://www.linkedin.com/in/arfat03/"
+            className="ml-[-100] flex justify-between items-center"
+            rel="linkedin noreferrer"
+            target={'_blank'}
+          >
+            {' '}
+            Linked <FaLinkedinIn size={30} />{' '}
+          </a>
+        </li>
+        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300 bg-zinc-800 py-2 ">
+          <a
+            href="https://github.com/ARFAT-HASAAN"
+            className="flex justify-between items-center "
+            rel="github noreferrer"
+            target="_blank"
+          >
+            {' '}
+            Github <FaGithub size={30} />{' '}
+          </a>
+        </li>
+        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300 py-2 bg-lime-600 ">
+          <a
+            href="mailto:Mr.Arafat.Hasan345@gmail.com"
+            className="flex justify-between items-center "
+          >
+            Eamil
+            <FaEnvelopeOpen size={30} />
+          </a>
+        </li>
+        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300  py-2 bg-purple-700 ">
+          <a
+            href="https://docs.google.com/document/d/1U2bTdQ5hU-_LkVxjUMQzPQYoOp-jzYo9FaeZWgNDcOw/edit?usp=sharing"
+            className="flex justify-between items-center "
+            rel="resume noreferrer"
+            target="_blank"
+          >
+            {' '}
+            Resume <FaFile size={30} />{' '}
+          </a>
+        </li>
+      </div>
       <Navbar></Navbar>
+
+      {/*
       <Banner></Banner>
       <Aboute></Aboute>
       <Skills></Skills>
       <Work></Work>
       <Contact></Contact>
-      <Footer></Footer>
+      <Footer></Footer> */}
     </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,122 +15,157 @@ const Navbar = () => {
   const Manubar = () => setNav(!nav)
 
   return (
-    <div className="bg-gray-900 h-[70px] px-2 md:px-36 fixed w-full flex justify-between items-center text-gray-300 ">
-      {/* nav brand name  */}
-      <div>
-        <img src={logo} style={{ width: '60px' }} alt="logo" />
-      </div>
+    <>
+      <section className="contaienr">
+        <div className="flex-col">
+          <div className="h-[70px] py-5">
+            <nav className="flex justify-between items-center ">
+              {/* logo  */}
+              <div>
+                <img src={logo} style={{ width: '60px' }} alt="logo" />
+              </div>
 
-      {/* nav item  */}
-      <ul className="hidden md:flex">
-        <Link to="home" smooth={true} duration={900}>
-          <li>Home</li>{' '}
-        </Link>
-        <Link to="work" smooth={true} duration={900}>
-          {' '}
-          <li>work</li>{' '}
-        </Link>
-        <Link to="about" smooth={true} duration={900}>
-          {' '}
-          <li>About</li>{' '}
-        </Link>
-        <Link to="contact" smooth={true} duration={900}>
-          {' '}
-          <li>Contact</li>{' '}
-        </Link>
-        <Link to="skills" smooth={true} duration={900}>
-          {' '}
-          <li>Skills</li>{' '}
-        </Link>
-      </ul>
+              {/* manu  */}
+              <ul className="hidden md:flex text-red-700">
+                <Link
+                  className="hover:border-b-4 border-red-700 font-bold transition-all duration-200 "
+                  to="home"
+                  smooth={true}
+                  duration={900}
+                >
+                  <li>Home</li>{' '}
+                </Link>
+                <Link to="work" smooth={true} duration={900}>
+                  {' '}
+                  <li>work</li>{' '}
+                </Link>
+                <Link to="about" smooth={true} duration={900}>
+                  {' '}
+                  <li>About</li>{' '}
+                </Link>
+                <Link to="contact" smooth={true} duration={900}>
+                  {' '}
+                  <li>Contact</li>{' '}
+                </Link>
+                <Link to="skills" smooth={true} duration={900}>
+                  {' '}
+                  <li>Skills</li>{' '}
+                </Link>
+              </ul>
 
-      {/* humburger logo  */}
-      <div onClick={Manubar} className="md:hidden z-40 cursor-pointer">
-        {!nav ? (
-          <FaBars className="text-2xl " />
-        ) : (
-          <FaTimes className="text-2xl" color="black" />
-        )}
-      </div>
+              {/* hamburger  */}
 
-      {/* mobile manus */}
-
-      <ul
-        className={
-          !nav
-            ? 'hidden'
-            : 'w-full h-screen absolute top-0 left-0 flex flex-col justify-center items-center z-1 text-gray-900 bg-slate-200 '
-        }
-      >
-        <Link to="home" onClick={Manubar} smooth={true} duration={900}>
-          <li className="my-3">Home</li>{' '}
-        </Link>
-        <Link to="work" onClick={Manubar} smooth={true} duration={900}>
-          {' '}
-          <li className="my-3">work</li>{' '}
-        </Link>
-        <Link to="about" onClick={Manubar} smooth={true} duration={900}>
-          {' '}
-          <li className="my-3">About</li>{' '}
-        </Link>
-        <Link to="contact" onClick={Manubar} smooth={true} duration={900}>
-          {' '}
-          <li className="my-3">Contact</li>{' '}
-        </Link>
-        <Link to="skills" onClick={Manubar} smooth={true} duration={900}>
-          {' '}
-          <li className="my-3">Skills</li>{' '}
-        </Link>
-      </ul>
-
-      {/* social manu  */}
-
-      <div className=" hidden  md:flex fixed flex-col left-0 top-80">
-        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300 py-2 bg-blue-500 ">
-          <a
-            href="https://www.linkedin.com/in/arfat03/"
-            className="ml-[-100] flex justify-between items-center"
-            rel="linkedin noreferrer"
-            target={'_blank'}
-          >
-            {' '}
-            Linked <FaLinkedinIn size={30} />{' '}
-          </a>
-        </li>
-        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300 bg-zinc-800 py-2 ">
-          <a
-            href="https://github.com/ARFAT-HASAAN"
-            className="flex justify-between items-center "
-            rel="github noreferrer"
-            target="_blank"
-          >
-            {' '}
-            Github <FaGithub size={30} />{' '}
-          </a>
-        </li>
-        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300 py-2 bg-lime-600 ">
-          <a
-            href="mailto:Mr.Arafat.Hasan345@gmail.com"
-            className="flex justify-between items-center "
-          >
-            Eamil
-            <FaEnvelopeOpen size={30} />
-          </a>
-        </li>
-        <li className="list-none w-[160px] h-[70] text-violet-50 ml-[-100px] hover:ml-[-10px] duration-300  py-2 bg-purple-700 ">
-          <a
-            href="https://docs.google.com/document/d/1U2bTdQ5hU-_LkVxjUMQzPQYoOp-jzYo9FaeZWgNDcOw/edit?usp=sharing"
-            className="flex justify-between items-center "
-            rel="resume noreferrer"
-            target="_blank"
-          >
-            {' '}
-            Resume <FaFile size={30} />{' '}
-          </a>
-        </li>
-      </div>
-    </div>
+              <div onClick={Manubar} className="md:hidden z-40 cursor-pointer">
+                {/* {!nav ? ( */}
+                <FaBars className="text-2xl text-white " />
+                {/* ) : ( */}
+                {/* <FaTimes className="text-2xl text-white" /> */}
+                {/* )} */}
+              </div>
+            </nav>
+          </div>
+          <div className="w-30  bg-slate-300">
+            <ul className="md:hidden flex-col text-red-700 bg-slate-900 ">
+              <Link to="home" smooth={true} duration={900}>
+                <li>Home</li>{' '}
+              </Link>
+              <Link to="work" smooth={true} duration={900}>
+                {' '}
+                <li>work</li>{' '}
+              </Link>
+              <Link to="about" smooth={true} duration={900}>
+                {' '}
+                <li>About</li>{' '}
+              </Link>
+              <Link to="contact" smooth={true} duration={900}>
+                {' '}
+                <li>Contact</li>{' '}
+              </Link>
+              <Link to="skills" smooth={true} duration={900}>
+                {' '}
+                <li>Skills</li>{' '}
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
 export default Navbar
+/* 
+
+<div className="bg-gray-900 h-[70px] px-2 md:px-36 fixed w-full flex justify-between items-center text-gray-300 ">
+    
+        <div>
+          <img src={logo} style={{ width: '60px' }} alt="logo" />
+        </div>
+
+      
+        <ul className="hidden md:flex">
+          <Link to="home" smooth={true} duration={900}>
+            <li>Home</li>{' '}
+          </Link>
+          <Link to="work" smooth={true} duration={900}>
+            {' '}
+            <li>work</li>{' '}
+          </Link>
+          <Link to="about" smooth={true} duration={900}>
+            {' '}
+            <li>About</li>{' '}
+          </Link>
+          <Link to="contact" smooth={true} duration={900}>
+            {' '}
+            <li>Contact</li>{' '}
+          </Link>
+          <Link to="skills" smooth={true} duration={900}>
+            {' '}
+            <li>Skills</li>{' '}
+          </Link>
+        </ul>
+
+       
+        <div onClick={Manubar} className="md:hidden z-40 cursor-pointer">
+          {!nav ? (
+            <FaBars className="text-2xl " />
+          ) : (
+            <FaTimes className="text-2xl" color="black" />
+          )}
+        </div>
+
+      
+        <ul
+          className={
+            !nav
+              ? 'hidden'
+              : 'w-full h-screen absolute top-0 left-0 flex flex-col justify-center items-center z-1 text-gray-900 bg-slate-200 '
+          }
+        >
+          <Link to="home" onClick={Manubar} smooth={true} duration={900}>
+            <li className="my-3">Home</li>{' '}
+          </Link>
+          <Link to="work" onClick={Manubar} smooth={true} duration={900}>
+            {' '}
+            <li className="my-3">work</li>{' '}
+          </Link>
+          <Link to="about" onClick={Manubar} smooth={true} duration={900}>
+            {' '}
+            <li className="my-3">About</li>{' '}
+          </Link>
+          <Link to="contact" onClick={Manubar} smooth={true} duration={900}>
+            {' '}
+            <li className="my-3">Contact</li>{' '}
+          </Link>
+          <Link to="skills" onClick={Manubar} smooth={true} duration={900}>
+            {' '}
+            <li className="my-3">Skills</li>{' '}
+          </Link>
+        </ul>
+
+      
+
+        
+      </div>
+
+  */
