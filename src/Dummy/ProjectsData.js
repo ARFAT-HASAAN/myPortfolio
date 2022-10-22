5284fd2f298faef835a4a67887f9900a0daeeeb1
ARFAT-HASAAN <arfuhtv757@gmail.com>
2022-10-22 12:25:05 +0600

    change

---
+++ src/Dummy/ProjectsData.js
@@ -0,0 +1,18 @@
+export const ProjectsData = [
+ {
+    Title: "Doctor Portal",
+    Live: "https://doctorportalweb.web.app/",
+    mockup:"https://i.ibb.co/TtLrJ4D/doctorportlat-Mockup-1.png",
+    desc: "This is an medical store , doctor portal is an. user can take an appoinment"
+ },{
+    Title: "Creative agency",
+    Live: "https://creativeagency-1b405.web.app/",
+    mockup : "https://i.ibb.co/dDvCptf/Image-3-1-1.png",
+    desc: "This is an medical store , doctor portal is an. user can take an appoinment"
+ }, {
+    Title: "Volunteer",
+    Live: "https://myvolunteerappi.web.app/",
+    mockup: "https://i.ibb.co/7vTkmhJ/Image-4-1-1.png",
+    desc: "This is an medical store , doctor portal is an. user can take an appoinment"
+ }
+]
--- src/components/Work.js
+++ src/components/Work.js
@@ -7,6 +7,7 @@ import MyProjects from './MyProjects'
 import swal from 'sweetalert'
 import { BiChevronsRight } from 'react-icons/bi'
 import { BiChevronsLeft } from 'react-icons/bi'
+import {ProjectsData} from "../Dummy/ProjectsData"
 import mockup from '../assets/realestate.jpg'
 
 export const isLoading = (state) => {
@@ -14,36 +15,53 @@ export const isLoading = (state) => {
 }
 
 const Work = () => {
-  const [Projects, setProjects] = useState([])
-  const [Loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    axios
-      .get('https://arfat-hasaan.github.io/API/Projects.json')
-      .then((res) => {
-        if (res.data) {
-          setLoading(true)
-          isLoading(true)
-          setProjects(res.data)
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-        if (error.message) {
-          swal({
-            icon: 'warning',
-            title: 'Opps',
-            text:
-              'Connectino dropped! Please check your internet connecton or reload the page',
-            buttons: 'Reload',
-          }).then((res) => {
-            if (res) {
-              window.location.reload()
-            }
-          })
-        }
-      })
-  }, [])
+  const [current, setCurrent] = useState(0)
+  const length = ProjectsData.length
+  const Next = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const Prev = () => {
+    setCurrent(current === 0 ? length - 1: current - 1)
+  }
+
+  if(!Array.isArray(ProjectsData) || ProjectsData <= 0){
+    return null
+
+  }
+
+
+
+
+
+  // useEffect(() => {
+  //   axios
+  //     .get('https://arfat-hasaan.github.io/API/Projects.json')
+  //     .then((res) => {
+  //       if (res.data) {
+  //         setLoading(true)
+  //         isLoading(true)
+  //         setProjects(res.data)
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //       if (error.message) {
+  //         swal({
+  //           icon: 'warning',
+  //           title: 'Opps',
+  //           text:
+  //             'Connectino dropped! Please check your internet connecton or reload the page',
+  //           buttons: 'Reload',
+  //         }).then((res) => {
+  //           if (res) {
+  //             window.location.reload()
+  //           }
+  //         })
+  //       }
+  //     })
+  // }, [])
 
   return (
     <>
@@ -54,36 +72,42 @@ const Work = () => {
           </h1>
           <div className="flex w-[100px] gap-4">
             <BiChevronsLeft
+              onClick={Next}
               className="bg-[#F51613] rounded"
               size={25}
               color={'white'}
             />
             <BiChevronsRight
+              onClick={Prev}
               className="bg-[#F51613] rounded"
               size={25}
               color={'white'}
             />
           </div>
         </div>
-
-        <div className="flex flex-col md:flex-row w-full justify-start my-12  shadow-md shadow-white ">
-          <div className="flex-1">
-            <img src={mockup} alt="" />
+        {
+          ProjectsData.map((project, index) => ( 
+            <div key={index} className={index === current ? "slide active " : "slide "}>
+              <div className='flex flex-col md:flex-row w-full justify-start my-12  shadow-md shadow-white'>
+              <div className="flex-1">
+            <img src={project.mockup} alt="" />
           </div>
           <div className=" flex flex-1 justify-center items-center flex-col text-left text-white px-4 py-2">
-            <h1 className="text-3xl font-extrabold mb-4">Product Title</h1>
+            <h1 className="text-3xl font-extrabold mb-4">{project.Title}</h1>
             <p className="text-[20px] w-4/5  text-center my-8 ">
               {' '}
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Sapiente, eveniet ad? Error rerum nam nobis totam nulla ex
-              blanditiis fuga, exercitationem quasi consectetur dignissimos
-              expedita sapiente corporis culpa voluptatem distinctio.{' '}
+              {project.desc}
             </p>
             <button className="py-2 px-6 border w-[260px] mx-auto border-[#F51613] font-extrabold text-2xl hover:bg-[#F51613] text-white transition-colors duration-100 ">
-              Live Preview
+              <a  href={project.Live} >Live Preview </a>
             </button>
           </div>
+              </div>
+         
         </div>
+          )) 
+        }
+        
       </section>
     </>
   )
